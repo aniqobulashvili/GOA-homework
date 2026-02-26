51490c1e18dc03590d23155e8d8122d4d2162245
aniqobulashvili <kartofilimwvanea@gmail.com>
2026-02-26 21:03:31 +0400

    first commit

---
+++ Level 06/homework/hw.py
@@ -0,0 +1,27 @@
+#type ფუნქცია გვეხმარება რომ გავიგოთ რომელ კატეგორიას მიეკუთვნება ცვლადი მაგალითად:
+num1 = 20
+print(type(num1))
+num2 = 17.43
+print(type(num2))
+name = "ani"
+print(type(name))
+
+name = "ani"
+surname = "qobulashvili"
+age = 15
+
+print('I am', name, surname, 'and i am', age, 'years old')
+
+num1 = 17.5
+num2 = 12.9
+print(num1 + num2)
+print(num2 - num1)
+print(num1 * num2)
+print(num1 / num2)
+
+num1 = 70
+num2 = 95
+num3 = 55
+num4 = 10
+num5 = 95
+print((num1 + num2 + num3 + num4 + num5) / 5)
