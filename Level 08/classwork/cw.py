d7b72ed7a907c908a2c313255412502ec8ab282f
aniqobulashvili <kartofilimwvanea@gmail.com>
2026-03-08 14:59:34 +0400

    first commit

---
+++ Level 08/classwork/cw.py
@@ -0,0 +1,18 @@
+print(True and False)
+print(True and True)
+print(False and False)
+print(False and True)
+print(True or True)
+print(True or False)
+print(False or False)
+print(False or True)
+
+print(True and False or False or True and True and False or True)  
+#        False     True      True     True    False      True  
+
+
+Family_member = 4
+people = int(input("How many people are in the house"))
+people > Family_member 
+Print = int(input("There is a thief in the house"))
+print(Family_member >= people)
---
+++ Level 08/homework/hw.py
@@ -0,0 +1,44 @@
+#<,>,==,<=,>=,!=
+print(67<45)
+print(3<40)
+
+print(54>20)
+print(20>75)
+
+print(20==20)
+print(45==45)
+
+print(80<=35)
+print(6<=10)
+
+print(25>=25)
+print(9>=12)
+
+print(30!=19)
+print(75!=75)
+
+True and False
+#and ჭეშმარიტი იქნება მაშინ თუ ორივე პირობა მსგავსია
+True or False
+#or ჭეშმარიტი იქნება თუ ერთ ერთი მაინც მართალია
+height1=1.81
+height2=1.64
+print(height2>=height1)
+#მომხარებელი არ არის ჩემზე მაღალი 
+
+num1="21"
+num2=21
+print(num1==num2)
+#ეს კოდი ტერმინალში False-ს იმიტომ გამოიტანს რომ String-ს ვერ გავუტოლებთ Intager-ს
+
+Lastname1="Qobulashvili"
+lastname2="aaoaooa"
+print(Lastname1==lastname2)
+#ორივე გვარი არ ემთხვევა ერთმანეთს
+
+False or True and True and False
+#გამოიტანს False
+True and False or False or True
+#გამოიტანს True
+True or True and False or True or False and True or False
+#გამოიტანს True
